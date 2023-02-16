3fb6cf757c2c8830ae341c0632a04e612744ced2
Shaoxuan_gitlab <swang878@wisc.edu>
2023-02-16 12:54:29 -0600

    add graphgl

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import React, { useState, useEffect } from 'react'; 
+import './App.css'; 
+import { API } from 'aws-amplify'; 
+import { withAuthenticator } from '@aws-amplify/ui-react'; 
+import { listTodos } from './graphql/queries'; 
+import { createTodo as createNoteMutation, deleteTodo as deleteNoteMutation } from './graphql/mutations';
+import { Storage } from 'aws-amplify';
 
-function App() {
-  const { signOut } = useAuthenticator()
+const initialFormState = { name: '', description: '' }
+
+function App() {   const [notes, setNotes] = useState([]);   const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();   }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listTodos });
+    const notesFromAPI = apiData.data.listTodos.items;
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
+    setNotes(apiData.data.listTodos.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});   }
+  
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
+  }
+
   return (
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-        <button onClick={() => signOut()}>Log Out</button>
-      </header>
-    </div>
-  );
-}
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <input
+        type="file"
+        onChange={onChange}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+      {
+      notes.map(note => (
+        <div key={note.id || note.name}>
+        <h2>{note.name}</h2>
+        <p>{note.description}</p>
+        <button onClick={() => deleteNote(note)}>Delete note</button>
+        {
+          note.image && <img src={note.image} style={{width: 400}} />
+        }
+      </div>
+      ))
+      }
+      </div>
+      <withAuthenticator />
+    </div>   ); }
 
 export default withAuthenticator(App);
+
